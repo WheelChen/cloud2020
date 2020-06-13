932cfc7c60f7df5874be62e892df24e903b1af4a
wheelchen <15088558209@163.com>
2020-06-13 16:59:26 +0800

    将订单服务与支付服务整合进入Eureka中

--- cloud-consumer-order-80/src/main/java/me/wheelchen/springcloud/OrderApplication.java
+++ cloud-consumer-order-80/src/main/java/me/wheelchen/springcloud/OrderApplication.java
@@ -2,11 +2,13 @@ package me.wheelchen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Kelvin Chen
  * @date 2020-06-13 10:31:19
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class OrderApplication {
     public static void main(String[] args) {
