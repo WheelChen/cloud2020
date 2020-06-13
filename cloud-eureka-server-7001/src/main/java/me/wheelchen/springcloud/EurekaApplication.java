13fed74c2ef01647cdaa4b211f5c469189ba1f0f
wheelchen <15088558209@163.com>
2020-06-13 16:32:55 +0800

    添加Eureka注册中心

---
+++ cloud-eureka-server-7001/src/main/java/me/wheelchen/springcloud/EurekaApplication.java
@@ -0,0 +1,17 @@
+package me.wheelchen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Kelvin Chen
+ * @date 2020-06-13 16:28:58
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
